1fc7a7b6234b35e2150347addf6081de251c666d
siriuska <you@example.com>
2022-04-07 18:14:58 +0200

    Split, reverse and join method

---
+++ ex02/src/index.js
@@ -0,0 +1,23 @@
+var myStr = "Paragon is the best!";
+
+function useMethods(str) {
+
+    var myReverse;
+    var countCharacters;
+
+    var words = str.split(" ");
+
+    words = words.reverse();
+
+    countCharacters = words.map((y) => y.length);
+
+    words = words.map((z) => z.split("").reverse().join(""));
+
+    myReverse = words.join(" ")
+
+    return { myReverse, countCharacters }
+}
+
+console.log(useMethods(myStr));
+
+module.exports = useMethods;
