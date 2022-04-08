2ad08b0b8441f98bb60f3b597e165a4abfde8426
siriuska <you@example.com>
2022-04-08 04:54:04 +0200

    Splice

---
+++ ex11/src/index.js
@@ -0,0 +1,16 @@
+function mySplice(arr1, arr2, n) {
+
+    let localArray = arr2.slice();
+    for (let i = 0; i < arr1.length; i++) {
+        localArray.splice(n, 0, arr1[i]);
+        n - 1;
+    }
+    return localArray;
+
+}
+console.log(mySplice([1, 2, 3], [4, 5], 1));
+console.log(mySplice([1, 2, 3], [4, 5], 2));
+console.log(mySplice(["b", "c"], ["a", "d"], 1));
+console.log(mySplice(["Dell", "Philips"], ["Samsung", "AOX", "Sony"], 2));
+
+module.exports = mySplice;
